2a9b59ce23ebf6333ca9f06675e029811012eefe
Arbyusman <acingman13@gmail.com>
2025-03-02 00:54:29 +0800

    feat: helpers traits

---
+++ app/Traits/LogTrait.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Traits;
+
+use App\Models\ActivityLog;
+
+trait LogTrait
+{
+    public function addLog($subject, $table = null, $table_id = null, $interface = 'web')
+    {
+        $request = app('request');
+
+        $type = match ($request->method()) {
+            'POST' => $request->is('login') ? 'login' : ($request->is('logout') ? 'logout' : 'add'),
+            'PUT' => 'edit',
+            default => 'delete'
+        };
+
+        if (strpos($table_id, ',')) {
+            $table_id = '"'.$table_id.'"';
+        }
+
+        ActivityLog::create([
+            'subject' => $subject,
+            'url' => $request->url(),
+            'type' => $type,
+            'table' => $table,
+            'table_id' => $table_id,
+            'user_id' => auth()->user()?->id ?? 1,
+        ]);
+    }
+}
