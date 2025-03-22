8a46f393c607bd2d78112045a969fee59d290d8c
Arbyusman <acingman13@gmail.com>
2025-03-23 02:29:47 +0800

    fix: update setting

--- app/Http/Controllers/SettingController.php
+++ app/Http/Controllers/SettingController.php
@@ -33,7 +33,7 @@ class SettingController extends Controller
         $validator = Validator::make($request->all(), [
             'name' => 'nullable|string|max:255',
             'short_name' => 'nullable|string|max:255',
-            'address' => 'nullable|string|max:255',
+            'address' => 'nullable|string',
             'email' => 'nullable|email|max:255',
             'phone' => 'nullable|string|max:255',
             'opening_hour' => 'nullable|string|max:255',
